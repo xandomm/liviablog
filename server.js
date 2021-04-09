fb8bc8d3f14ff9933b9a2cf28a5b74abcdfbc0fa
Alexandre Machado <xandemachado2@hotmail.com>
2021-04-09 15:16:27 -0300

    dsa

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 
 app.use(cors())
